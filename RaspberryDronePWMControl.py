0afb504aa4775669b77c6a14e916be6087cfad2a
jsn3111 <54827199+jsn3111@users.noreply.github.com>
2024-10-16 09:42:00 +0900

    Colab을 통해 생성됨

---
+++ RaspberryDronePWMControl.py
@@ -0,0 +1,48 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyMcbY6c3s5YYQqBNEhmIFAC",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/jsn3111/20240805-01/blob/main/RaspberryDronePWMControl.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "6RPI4czHvLzr"
+      },
+      "outputs": [],
+      "source": [
+        "import RPi.GPIO as GPIO\n",
+        "import time\n",
+        "\n",
+        "# GPIO 설정\n",
+        "GPIO.setmode(GPIO.BCM)\n",
+        "GPIO.setwarnings(False)\n",
+        "\n",
+        "# ESC 신호핀 설정\n"
+      ]
+    }
+  ]
+}
